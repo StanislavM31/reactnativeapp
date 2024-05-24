783b9cdced094b32b90bc48b6f05632d102bcdc0
Stanislav M <minkostas@gmail.com>
2024-05-24 19:55:52 +0300

    auth,reg,home

--- mobile-auth-form/App.js
+++ mobile-auth-form/App.js
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import Home from './scr/screen/Home/Home'
-import Auth from './scr/screen/Auth/Auth'
-import Reg from './scr/screen/Reg/Reg'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './scr/screen/Home/Home';
+import Auth from './scr/screen/Auth/Auth';
+import Reg from './scr/screen/Reg/Reg';
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 export default function App() {
-
   return (
     <NavigationContainer>
-      <StackRouter.Navigator initialRouteName='Home'/>
-      <Stack.Screen name='Home'component={Home}/>
-      <Stack.Screen name='Sign In'component={Auth}/>
-      <Stack.Screen name='Sign Up'component={Reg}/>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='Sign In' component={Auth} />
+        <Stack.Screen name='Sign Up' component={Reg} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
---
+++ mobile-auth-form/scr/screen/Auth/style.js
@@ -0,0 +1,17 @@
+import { StyleSheet} from 'react-native';
+
+const style = StyleSheet.create({
+    container:{
+        width: '100%',
+        height: '100%',
+        backgroundColor: '#F0F4F3',
+        alignItems: 'center',
+    }, inp:{
+        borderRadius:"2px solid #E2E8FO",
+        border: '80%',
+        width:'80%',
+        paddingHorizontal:16,
+        paddingVertical:16
+    }
+})
+export default styles
