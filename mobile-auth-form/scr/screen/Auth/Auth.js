2af15c4b67efb7a53f055dc367a67a5ba04f960e
Stanislav Minko <minkostas@gmail.com>
2024-05-27 14:07:33 +0300

    :validate inputs

--- mobile-auth-form/scr/screen/Auth/Auth.js
+++ mobile-auth-form/scr/screen/Auth/Auth.js
@@ -33,12 +33,24 @@ export default function Auth({navigation}){
             {...valueInp, [name] : value }
         )
     }
+    const checkInps = () => {
+		try {
+			if (!valueInp.email.trim()) throw new Error('Пустое поле  email')
+			if (!/^[A-z0-9\.\+\_\-]+@[a-z\.]+\.{1}[a-z]{2,4}$/gm.test(valueInp.email)) throw new Error('Email is invalid')
+			if (!valueInp.password.trim()) throw new Error('Пустое поле Password')
+
+
+			console.log(`Success ${valueInp}`);
+		} catch (error) {
+			console.log(error.message);
+		}
+	}
     return (
         <View style = {styles.container}>
             <TextInput style={styles.inp} onChangeText={(value)=> addValue(value,'email')} placeholder="email"/>
             <TextInput style={styles.inp} onChangeText={(value)=> addValue(value, 'password')} placeholder="password" secureTextEntry={true}/>
             <Text style={styles.no_acc}>Нет акаунта? <Text style={styles.no_acc_link} onPress={()=>navigation.navigate('Sign Up')}>Sign Up (Зарегистрироваться)</Text></Text>
-            <Button title="Login"></Button>
+            <Button title="Login" onPress={checkInps}></Button>
         </View>
     )
 }
